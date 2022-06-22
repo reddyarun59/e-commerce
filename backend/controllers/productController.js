dc2d6a14a6a447f0f1af5e616d5a774a3eab1bc6
reddyarun59 <reddyarun59@yahoo.com>
2022-06-22 23:58:34 +0530

    Added product route and where search clause

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,10 +1,45 @@
 const asyncHandler = require("express-async-handler")
+const Product = require("../models/product")
+const cloudinary= require("cloudinary")
+//const product = require("../models/product")
 
-const dummy=asyncHandler(async(req, res)=>{
-    res.status(200).json({message:"Hello"})
+
+const addProduct = asyncHandler(async(req, res)=>{
+    
+    let imageArray=[]
+
+    if(!req.files){
+        res.status(401)
+        throw new Error("Images are required")
+    }
+
+    if(req.files){
+
+        for(let index = 0; index < req.files.photos.length; index++){
+            let result = await cloudinary.v2.uploader.upload(
+                req.files.photos[index].tempFilePath,
+                {
+                    folder:"products"
+                }
+            )
+            imageArray.push({
+                id:result.public_id,
+                secure_url:result.secure_url
+            })
+        }
+    }
+
+    req.body.photos=imageArray
+    req.body.user=req.user.id
+
+    const product=await Product.create(req.body)
+
+    res.status(200).json({
+        success:true,
+        product
+    })
 })
 
-
 module.exports={
-    dummy
+    addProduct
 }
