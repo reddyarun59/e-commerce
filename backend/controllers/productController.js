6e420fb5b0b8587f8098d525206a32695b8cf99c
reddyarun59 <reddyarun59@yahoo.com>
2022-06-23 15:00:19 +0530

    Added admin products controller

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -66,7 +66,110 @@ const getAllProduct= asyncHandler(async(req, res)=>{
   });
 })
 
+const getOneProduct= asyncHandler(async(req, res)=>{
+    
+    const product = await Product.findById(req.params.id)
+
+    if(!product){
+        res.status(401)
+        throw new Error("No product found with this ID")
+    }
+
+    if(product){
+        res.status(200).json({
+            success: true,
+            product,
+        })
+    }
+})
+
+const adminGetAllProduct = asyncHandler(async(req, res)=>{
+
+    const products = await Product.find()
+
+    res.status(200).json({
+        success: true,
+        products,
+    })
+})
+
+const adminUpdateOneProduct = asyncHandler(async(req, res)=>{
+
+    let product = await Product.findById(req.params.id)
+
+    let imagesArray=[]
+
+    if(!product){
+        res.status(401)
+        throw new Error("No product found with this ID")
+    }
+
+    if(req.files){
+
+        //destroy the existing Images
+        for(let index=0; index<product.photos.length; index++){
+          const result = await cloudinary.v2.uploader.destroy(product.photos[index].id)
+        }
+
+        //upload and save the images
+
+        for(let index = 0; index < req.files.photos.length; index++){
+            let result = await cloudinary.v2.uploader.upload(
+                req.files.photos[index].tempFilePath,
+                {
+                    folder:"products"
+                }
+            )
+            imagesArray.push({
+                id:result.public_id,
+                secure_url:result.secure_url
+            })
+        }
+
+    }
+
+    req.body.photos=imagesArray
+    
+    product= await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new:true,
+        runValidators: true
+    })
+
+    res.status(200).json({
+        success:true,
+        product
+    })
+})
+
+const adminDeleteOneProduct= asyncHandler(async(req, res)=>{
+
+    const product= await Product.findById(req.params.id)
+
+    if(!product){
+        res.status(401)
+        throw new Error("No product found with this ID")
+    }
+
+    //destroy the existing Images
+    for(let index=0; index<product.photos.length; index++){
+        const result = await cloudinary.v2.uploader.destroy(product.photos[index].id)
+    }
+
+    await product.remove();
+
+    res.status(200).json({
+        success: true,
+        message:"Product was successfully deleted"
+    })
+
+})
+
 module.exports={
     addProduct,
-    getAllProduct
+    getAllProduct,
+    getOneProduct,
+    adminGetAllProduct,
+    adminUpdateOneProduct,
+    adminDeleteOneProduct,
 }
+
